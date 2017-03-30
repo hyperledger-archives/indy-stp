8ae7ed58a394a4c7ab1825042a4521664c807e97
Victor Muzychenko <mzkvct@gmail.com>
2017-03-30 16:39:33 +0300

    add connection-related exceptions from plenum

--- stp_core/network/exceptions.py
+++ stp_core/network/exceptions.py
@@ -14,3 +14,28 @@ class DuplicateRemotes(Exception):
         msg = "Found remotes {}: {}". \
             format(len(remotes), [(r.name, r.ha) for r in remotes])
         super(DuplicateRemotes, self).__init__(msg)
+
+
+class EndpointException(Exception):
+    pass
+
+
+class MissingEndpoint(EndpointException):
+    def __init__(self):
+        super().__init__('missing endpoint')
+
+
+class InvalidEndpointIpAddress(EndpointException):
+    def __init__(self, endpoint):
+        super().__init__("invalid endpoint address: '{}'".format(endpoint))
+
+
+class InvalidEndpointPort(EndpointException):
+    def __init__(self, endpoint):
+        super().__init__("invalid endpoint port: '{}'".format(endpoint))
+
+
+class PortNotAvailable(OSError):
+    def __init__(self, port):
+        self.port = port
+        super().__init__("port not available: {}".format(port))
