1935a997ddde66cca58768400ac7e705d7e3be86
Devin Fisher <devin.fisher@evernym.com>
2017-04-02 21:08:06 -0600

    Added a run_till_quiet, waits for quiet prods
    This function will wait till there is a quiet period from the
    prodables. Dependent on prodables correctly reporting events.
    Will run forever if there is always activity.

--- stp_core/loop/looper.py
+++ stp_core/loop/looper.py
@@ -4,8 +4,8 @@ import signal
 import sys
 import time
 from asyncio import Task
-from asyncio.coroutines import CoroWrapper
 from typing import List
+from collections import deque
 
 # import uvloop
 from stp_core.common.log import getlogger
@@ -78,6 +78,7 @@ class Looper:
         :param debug: set_debug on event loop will be set to this value
         :param autoStart: start immediately?
         """
+        self._activity = deque(maxlen=10)
         self.prodables = list(prodables) if prodables is not None \
             else []  # type: List[Prodable]
 
@@ -85,8 +86,10 @@ class Looper:
         #     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
         if loop:
+            self.manage_loop = False
             self.loop = loop
         else:
+            self.manage_loop = True
             try:
                 #if sys.platform == 'win32':
                 #    loop = asyncio.ProactorEventLoop()
@@ -144,7 +147,10 @@ class Looper:
         limit = None
         s = 0
         for n in self.prodables:
-            s += await n.prod(limit)
+            e = await n.prod(limit)
+            if e > 0:
+                self._activity.appendleft((time.time(), e))
+            s += e
         return s
 
     def add(self, prodable: Prodable) -> None:
@@ -205,6 +211,45 @@ class Looper:
             logger.info("it took {:.3f} seconds to run once nicely".
                         format(dur), extra={"cli": False})
 
+    def run_till_quiet(self, quiet_for=1, timeout=None):
+        """
+        Runs till the Prodable are quite (not processing events)
+
+        :param quiet_for: How long should activity be quiet for
+        :param timeout: A max timeout that should be waited
+
+        :return: No return
+        """
+        start_time = time.time()
+
+        self.runFor(.01)  # allow prodables to run before checking
+        while True:
+            if timeout is not None and \
+               time.time() - start_time > timeout:
+                break
+
+
+            last_activity = None
+            if len(self._activity) > 0:
+                last_activity = self._activity[0]
+
+            if last_activity:
+                inactive_for = time.time() - last_activity[0]
+                if inactive_for > quiet_for:
+                    # no activity for the required time
+                    return
+                else:
+                    # too busy, wait more
+                    run_for = quiet_for - inactive_for
+                    self.runFor(run_for)
+                    continue
+            else:
+                # Nothing has ever happened
+                return
+
+        # timeout has been passed, return
+        return
+
     def runFor(self, timeout):
         self.run(asyncio.sleep(timeout))
 
@@ -215,7 +260,7 @@ class Looper:
         while self.running:
             await self.runOnceNicely()
 
-    def run(self, *coros: CoroWrapper):
+    def run(self, *coros):
         """
         Runs an arbitrary list of coroutines in order and then quits the loop,
         if not running as a context manager.
@@ -275,7 +320,8 @@ class Looper:
         return self
 
     def shutdownSync(self):
-        self.loop.run_until_complete(self.shutdown())
+        if self.manage_loop:
+            self.loop.run_until_complete(self.shutdown())
 
     # noinspection PyUnusedLocal
     def __exit__(self, exc_type, exc_val, exc_tb):
